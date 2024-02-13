98f34a9cb3ed9cc634d4719e0ea596445b76fb1c
ankit-srivas <ascontinentalgt@gmail.com>
2024-02-13 11:23:05 -0500

    Initial Commit

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,3 @@
+var Sent1 = "This is the beginning of the string", Sent2 = " and this is the end of the string"; //Assigning variables
+var con = Sent1 + Sent2; //Concatenating 2 strings
+document.write(con); //Printing the result on the webpage
